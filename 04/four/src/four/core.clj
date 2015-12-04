0326059906735df01c8f8001e76694499d585868
Raimon Grau <raimon@3scale.net>
2015-12-04 13:06:02 +0000

    days 3 and 4
    * 3 in lua
    * 4 in clojure

---
+++ 04/four/src/four/core.clj
@@ -0,0 +1,16 @@
+(ns four.core
+  (:require [digest])
+  (:gen-class))
+
+(def key "bgvyzdsv")
+
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [count]
+  (require 'digest)
+  (loop [k key n 0]
+    (if ( =  (clojure.string/join (repeat count "0")) (subs (digest/md5 (str k n)) 0 count))
+      (println n)
+      (recur key (inc n)))))
